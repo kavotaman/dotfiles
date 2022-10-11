b3c51057ac6173c35319865677af08fc4ea5a208
kavotaman <otaviokavakama@gmail.com>
2022-10-11 11:56:40 -0500

    updatessss

---
+++ .config/nwg-wrapper/todo.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+todo=$(cat /home/omk/Documents/todo.txt)
+
+echo '<span size="12000" foreground="#ccc">'"$todo"'</span>'
--- .config/nwg-wrapper/wttr.sh
+++ .config/nwg-wrapper/wttr.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 
-todo=$(cat /home/omk/Documents/todo.txt)
 wttr2=$(curl wttr.in/43402?format="%C+%t++%f\n")
 wttr3=$(curl wttr.in/43402?format="++%w+++%P\n")
 wttr4=$(curl wttr.in/43402?format="懲+%h++爫+%p\n")
 
 echo '<span size="25000" foreground="#ccc">'$wttr2'</span><span size="20000" foreground="#ccc">'
 echo $wttr3'</span><span size="18000" foreground="#ccc">'
-echo $wttr4'</span><span size="11500" foreground="#ccc">'
-echo -en '\n'
-echo "$todo"'</span>'
+echo $wttr4'</span>'
